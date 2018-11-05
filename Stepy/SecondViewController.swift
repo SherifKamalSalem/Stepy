01156e11a600f53d6c69198e78d51127842e731e
Sherif Kamal <sherif.kamal.salem@gmail.com>
2018-11-05 08:51:55 +0200

    Initial Commit

---
+++ Stepy/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  Stepy
+//
+//  Created by Sherif Kamal on 11/5/18.
+//  Copyright © 2018 Sherif Kamal. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
