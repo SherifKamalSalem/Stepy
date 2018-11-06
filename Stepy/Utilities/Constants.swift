35d55316bf0cd0bbe45a9b3ac0451a2f83604690
Sherif Kamal <sherif.kamal.salem@gmail.com>
2018-11-07 00:40:22 +0200

    Add and return data from Realm

---
+++ Stepy/Utilities/Constants.swift
@@ -0,0 +1,11 @@
+//
+//  Constants.swift
+//  Stepy
+//
+//  Created by Sherif Kamal on 11/7/18.
+//  Copyright © 2018 Sherif Kamal. All rights reserved.
+//
+
+import Foundation
+
+let REALM_QUEUE = DispatchQueue(label: "realmQueue")
